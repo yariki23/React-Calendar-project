1fae9c9c26e312c44750ea328f6d6248ec182f0a
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-07-07 12:39:55 +0300

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+  const isProduction = argv.mode === "production";
   const config = {
-    entry: './src/index.jsx',
+    entry: "./src/index.jsx",
     output: {
-      filename: 'bundle.js',
+      filename: "bundle.js",
     },
     module: {
       rules: [
         {
           test: /.(js|jsx?)$/,
           exclude: /node_modules/,
-          use: ['babel-loader'],
+          use: ["babel-loader"],
         },
         {
           test: /.s?css$/,
           use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-            'sass-loader',
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
           ],
         },
       ],
     },
     resolve: {
-      extensions: ['.js', '.jsx'],
+      extensions: [".js", ".jsx"],
     },
     plugins: [
       new webpack.ProgressPlugin(),
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: "./src/index.html",
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "_redirects", to: "" },
+          { from: "source/url/to/images/folder", to: "dest/url/to/images/folder" },
+        ],
       }),
     ],
     devServer: {
@@ -52,8 +59,8 @@ module.exports = (env, argv) => {
   if (isProduction) {
     config.plugins.push(
       new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
+        filename: "[name].css",
+      })
     );
   }
 
